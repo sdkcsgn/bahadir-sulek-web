3061e944d77459cd62191c379e023f0fa06573e3
Sadik Cosgun <cosgun.sadik@gmail.com>
2026-09-21 16:38:00 +0300

    Ana sayfayi dinamik yap

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/prisma/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const siteContent = await prisma.siteContent.findFirst();
 
